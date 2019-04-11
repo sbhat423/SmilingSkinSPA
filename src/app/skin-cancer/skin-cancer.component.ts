a9ef6725d2ef2b660933fdcdf60e64ad268d9095
sharat <sharattbhat@gmail.com>
2019-04-11 14:48:10 +1000

    Improve sidenav

--- src/app/skin-cancer/skin-cancer.component.ts
+++ src/app/skin-cancer/skin-cancer.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skin-cancer.component.css']
 })
 export class SkinCancerComponent implements OnInit {
+  links: boolean[] = [false, false, false, false, false, false, false, false];
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
   }
 
+  setColor(loc: number) {
+    for ( let i = 0; i < 8; i++) {
+      this.links[i] = false;
+    }
+    this.links[loc] = true;
+    console.log(this.links[loc]);
+  }
 }
