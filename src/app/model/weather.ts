e6f2c2b3a362295cc956281923bdcd533db659dd
sharat <sharattbhat@gmail.com>
2019-04-15 21:46:25 +1000

    Add weather model

---
+++ src/app/model/weather.ts
@@ -0,0 +1,21 @@
+export interface Weather {
+    time: number;
+    latitude: number;
+    longitude: number;
+    summary: string;
+    icon: string;
+    precipIntensity: number;
+    precipProbability: number;
+    temperature: number;
+    apparentTemperature: number;
+    dewPoint: number;
+    humidity: number;
+    pressure: number;
+    windSpeed: number;
+    windGust: number;
+    windBearing: number;
+    cloudCover: number;
+    uvIndex: number;
+    visibility: number;
+    ozone: number;
+}
