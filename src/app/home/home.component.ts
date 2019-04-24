1f7ff24e355ba3f935761f925f6603fb28fc9400
sharat <sharattbhat@gmail.com>
2019-04-24 15:57:22 +1000

    Display basic weather info on homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../service/weather.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor() { }
+  summary: string;
+  temperature: number;
+  apparentTemperature: number;
+  humidity: number;
+  cloudCover: number;
+  uvIndex: number;
+  ozone: number;
 
-  ngOnInit() { }
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.fetchCurrentData();
+     }, 1000);
+  }
+
+  fetchCurrentData() {
+    console.log('fetch current data');
+    this.weatherService.getWeatherData().subscribe(
+      data => {
+        this.summary = data['currently']['summary'];
+        this.temperature = data['currently']['temperature'];
+        this.apparentTemperature = data['currently']['apparentTemperature'];
+        this.humidity = data['currently']['humidity'];
+        this.cloudCover = data['currently']['cloudCover'];
+        this.uvIndex = data['currently']['uvIndex'];
+        this.ozone = data['currently']['ozone'];
+    });
+  }
 }
