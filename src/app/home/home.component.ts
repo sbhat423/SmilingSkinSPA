2a6c762988a0d33c2f400ffaa2ac6e065684e96d
sharat <sharattbhat@gmail.com>
2019-04-26 16:50:11 +1000

    Add function to store data to our backend server

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../service/weather.service';
+import { Weather } from '../model/weather';
+import { SkApiService } from '../service/sk-api.service';
 
 @Component({
   selector: 'app-home',
@@ -15,14 +17,21 @@ export class HomeComponent implements OnInit {
   uvIndex: number;
   ozone: number;
   degreeTemperature: String;
+  currentTime: number;
+  currentHour: number;
+  hourlyData: string[] = [];
 
-  constructor(private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService, private api: SkApiService) { }
 
   ngOnInit() {
     this.fetchCurrentData();
+    this.storeWeatherData();
     setInterval(() => {
       this.fetchCurrentData();
      }, 90000);
+     setInterval(() => {
+      this.storeWeatherData();
+     }, 600000);
   }
 
   fetchCurrentData() {
@@ -37,6 +46,40 @@ export class HomeComponent implements OnInit {
         this.cloudCover = data['currently']['cloudCover'];
         this.uvIndex = data['currently']['uvIndex'];
         this.ozone = data['currently']['ozone'];
+    });
+  }
+
+  storeWeatherData() {
+    console.log('store data');
+    this.weatherService.getWeatherData().subscribe(
+      data => {
+        this.hourlyData = data['hourly']['data'];
+        for (let i = 0; i < 5; i++) {
+          const weather: Weather = {
+          latitude: data['latitude'],
+          longitude: data['longitude'],
+          time: this.hourlyData[i]['time'],
+          summary: this.hourlyData[i]['summary'],
+          icon: this.hourlyData[i]['icon'],
+          precipIntensity: this.hourlyData[i]['precipIntensity'],
+          precipProbability: this.hourlyData[i]['precipProbability'],
+          temperature: this.hourlyData[i]['temperature'],
+          apparentTemperature: this.hourlyData[i]['apparentTemperature'],
+          dewPoint: this.hourlyData[i]['dewPoint'],
+          humidity: this.hourlyData[i]['humidity'],
+          pressure: this.hourlyData[i]['pressure'],
+          windSpeed: this.hourlyData[i]['windSpeed'],
+          windGust: this.hourlyData[i]['windGust'],
+          windBearing: this.hourlyData[i]['windBearing'],
+          cloudCover: this.hourlyData[i]['cloudCover'],
+          uvIndex: this.hourlyData[i]['uvIndex'],
+          visibility: this.hourlyData[i]['visibility'],
+          ozone: this.hourlyData[i]['ozone']
+        };
+          this.api.postWeatherData(weather).subscribe(res => {
+            console.log(res);
+          });
+        }
     });
   }
 }
