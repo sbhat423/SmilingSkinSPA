26f89018c3ecdb6431f0e1fc13ea84a5f92c16a1
keenoro <kzkho1@student.monash.edu>
2019-05-22 00:33:18 +1000

    Fixed about skin page for desktop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,6 @@ import { CausesInfoComponent } from './Info-pages/causes-info/causes-info.compon
 import { SymptomsInfoComponent } from './Info-pages/symptoms-info/symptoms-info.component';
 import { DiagnosisInfoComponent } from './Info-pages/diagnosis-info/diagnosis-info.component';
 import { TreatmentInfoComponent } from './Info-pages/treatment-info/treatment-info.component';
-import { PreventionInfoComponent } from './Info-pages/prevention-info/prevention-info.component';
 import { Error404Component } from './error404/error404.component';
 import { HttpClientModule } from '@angular/common/http';
 import { WeatherComponent } from './weather/weather.component';
@@ -69,7 +68,6 @@ import { DisclaimerComponent } from './disclaimer/disclaimer.component';
     SymptomsInfoComponent,
     DiagnosisInfoComponent,
     TreatmentInfoComponent,
-    PreventionInfoComponent,
     Error404Component,
     WeatherComponent,
     PrenevtionComponent,
